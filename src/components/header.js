bebbf87d7cb509ecd920193b46ec758169954a2a
Chhorvy Om <c_om89@live.com>
2022-08-01 21:22:13 -0700

    Created header component in Task 1

--- src/components/header.js
+++ src/components/header.js
@@ -11,7 +11,25 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
-}
+    const headerWrapper = document.createElement("div");
+    const headerDate = document.createElement("span");
+    const headerTitle = document.createElement("h1");
+    const headerTemp = document.createElement("span");
+    
+    headerWrapper.appendChild(headerDate);
+    headerWrapper.appendChild(headerTitle);
+    headerWrapper.appendChild(headerTemp);
+
+    headerWrapper.classList.add("header");
+    headerDate.classList.add("date");
+    headerTemp.classList.add("temp");
+    
+    headerDate.textContent = date;
+    headerTitle.textContent = title;
+    headerTemp.textContent = temp;
+    
+    return headerWrapper;
+  }
 
 const headerAppender = (selector) => {
   // TASK 2
